c6f74ed180975f57cd12f1d317ba429dec42dfda
joaodenari <joao.denari@hotmail.com>
2022-09-02 20:07:32 -0300

    js-updated

--- js/javascript.js
+++ js/javascript.js
@@ -1,94 +1,76 @@
-projectNameList = ['Denari Blog', 'Metaseed', 'Counter Strike GO Website'];
+// Object with all the variables information
 
-projectFirstDescriptionList = ['Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.', 'The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software.', 'Richard McClintock, a Latin scholar from Hampden-Sydney College, is credited with discovering the source behind the ubiquitous filler text.']
+projectObject = {
+    
+    name: ['Denari Blog', 'Metaseed', 'Counter Strike GO Website'],
+    
+    firstDescription: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+    
+    'The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software.',
+    
+    'Richard McClintock, a Latin scholar from Hampden-Sydney College, is credited with discovering the source behind the ubiquitous filler text.'],
 
-projectSecondDescriptionList = ['The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software. Today it\'s seen all around the web; on templates, websites, and stock designs.', 'In particular, the garbled words of lorem ipsum bear an unmistakable resemblance to sections', 'The placeholder text, beginning with the line “Lorem ipsum dolor sit amet, consectetur adipiscing elit”, looks like Latin because in its youth, centuries ago, it was Latin.']
+    secondDescription: ['The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software. Today it\'s seen all around the web; on templates, websites, and stock designs.',
+    
+    'In particular, the garbled words of lorem ipsum bear an unmistakable resemblance to sections',
+    
+    'The placeholder text, beginning with the line “Lorem ipsum dolor sit amet, consectetur adipiscing elit”, looks like Latin because in its youth, centuries ago, it was Latin.'],
 
-iconTextDenariBlog = ['HTML', 'BOOTSTRAP', 'JAVASCRIPT']
+    firstIcon: ['HTML', 'HTML', 'HTML'],
 
-iconTextMetaseed = ['HTML', 'CSS', 'JAVASCRIPT']
+    secondIcon: ['BOOTSTRAP', 'CSS', 'CSS'],
 
-iconTextCounterStrikeGOWebsite = ['HTML', 'CSS', 'JAVASCRIPT']
+    thirdIcon: ['JAVASCRIPT', 'JAVASCRIPT', 'JAVASCRIPT'],
 
-var currentImgIndex = 0;
+    firstBar: ["width: 50%", "width: 50%", "width: 75%"],
+    
+    secondBar: ['width: 75%', 'width: 75%', 'width: 100%'],
 
-function addIndex(){
-    currentImgIndex ++;
-
-    if (currentImgIndex === 3){
-        currentImgIndex=0;
-    }
+    thirdBar: ['width: 25%', 'width: 100%', 'width: 50%'],
 }
 
-function subIndex(){
-    currentImgIndex --;
+// index to guide the changes
+currentImgIndex = 0;
+
+const addIndex = () => currentImgIndex++;
+const subIndex = () => currentImgIndex--;
+
+function checkIndex(){
+    if (currentImgIndex === 3){
+        currentImgIndex=0;
+    } 
     if (currentImgIndex === -1){
         currentImgIndex=2;
     }
 }
 
-function changeIndexto0(){
-    currentImgIndex=0;
-}
-
-function changeIndexto1(){
-    currentImgIndex=1;
-}
-
-function changeIndexto2(){
-    currentImgIndex=2;
-}
+// function if the user clicks on the bottom button
+const changeIndexto0 = () => currentImgIndex=0;
+const changeIndexto1 = () => currentImgIndex=1;
+const changeIndexto2 = () => currentImgIndex=2;
 
 function changeProject(){
 
-    // altera a imagem de fundo da primeira página
-    document.getElementById("title-project").innerHTML = projectNameList[currentImgIndex];
-    document.getElementById("first-description-project").innerHTML = projectFirstDescriptionList
-    [currentImgIndex];
-    document.getElementById("second-description-project").innerHTML = projectSecondDescriptionList
-    [currentImgIndex];
+    // project title
+    document.querySelectorAll(".title-project")[0].innerHTML = projectObject.name[currentImgIndex];
 
-    if(document.getElementById("title-project").innerHTML === 'Metaseed'){
+    // text description title
+    document.querySelectorAll(".description-project")[0].innerHTML = projectObject.firstDescription[currentImgIndex];
+    document.querySelectorAll(".description-project")[1].innerHTML = projectObject.secondDescription[currentImgIndex];
 
-        document.getElementById("first-icon-text-project").innerHTML = iconTextMetaseed[0];
-        document.getElementById("second-icon-text-project").innerHTML = iconTextMetaseed[1];
-        document.getElementById("third-icon-text-project").innerHTML = iconTextMetaseed[2];
+    // icon main skills
+    document.querySelectorAll(".icon-image-project")[0].src = `images/skills-icon/${projectObject.firstIcon[currentImgIndex].toLowerCase()}-icon.png`
+    document.querySelectorAll(".icon-image-project")[1].src = `images/skills-icon/${projectObject.secondIcon[currentImgIndex].toLowerCase()}-icon.png`
+    document.querySelectorAll(".icon-image-project")[2].src = `images/skills-icon/${projectObject.thirdIcon[currentImgIndex].toLowerCase()}-icon.png`
 
-        document.getElementById("first-icon-image-project").src = `images/skills-icon/${iconTextMetaseed[0].toLowerCase()}-icon.png`
-        document.getElementById("second-icon-image-project").src = `images/skills-icon/${iconTextMetaseed[1].toLowerCase()}-icon.png`
-        document.getElementById("third-icon-image-project").src = `images/skills-icon/${iconTextMetaseed[2].toLowerCase()}-icon.png`
+    // text icon main skills
+    document.querySelectorAll(".icon-text-project")[0].innerHTML = projectObject.firstIcon[currentImgIndex];
+    document.querySelectorAll(".icon-text-project")[1].innerHTML = projectObject.secondIcon[currentImgIndex];
+    document.querySelectorAll(".icon-text-project")[2].innerHTML = projectObject.thirdIcon[currentImgIndex];
 
-        document.getElementsByClassName("progress-bar")[0].style = "width: 50%";
-        document.getElementsByClassName("progress-bar")[1].style = "width: 75%";
-        document.getElementsByClassName("progress-bar")[2].style = "width: 25%";
-
-
-    } else if(document.getElementById("title-project").innerHTML === 'Counter Strike GO Website'){
-
-        document.getElementById("first-icon-text-project").innerHTML = iconTextCounterStrikeGOWebsite[0];
-        document.getElementById("second-icon-text-project").innerHTML = iconTextCounterStrikeGOWebsite[1];
-        document.getElementById("third-icon-text-project").innerHTML = iconTextCounterStrikeGOWebsite[2];
-
-        document.getElementById("first-icon-image-project").src = `images/skills-icon/${iconTextCounterStrikeGOWebsite[0].toLowerCase()}-icon.png`
-        document.getElementById("second-icon-image-project").src = `images/skills-icon/${iconTextCounterStrikeGOWebsite[1].toLowerCase()}-icon.png`
-        document.getElementById("third-icon-image-project").src = `images/skills-icon/${iconTextCounterStrikeGOWebsite[2].toLowerCase()}-icon.png`
-
-        document.getElementsByClassName("progress-bar")[0].style = "width: 50%";
-        document.getElementsByClassName("progress-bar")[1].style = "width: 75%";
-        document.getElementsByClassName("progress-bar")[2].style = "width: 100%";
-        
-    } else{
-
-        document.getElementById("first-icon-text-project").innerHTML = iconTextDenariBlog[0];
-        document.getElementById("second-icon-text-project").innerHTML = iconTextDenariBlog[1];
-        document.getElementById("third-icon-text-project").innerHTML = iconTextDenariBlog[2];
-
-        document.getElementById("first-icon-image-project").src = `images/skills-icon/${iconTextDenariBlog[0].toLowerCase()}-icon.png`
-        document.getElementById("second-icon-image-project").src = `images/skills-icon/${iconTextDenariBlog[1].toLowerCase()}-icon.png`
-        document.getElementById("third-icon-image-project").src = `images/skills-icon/${iconTextDenariBlog[2].toLowerCase()}-icon.png` 
-
-        document.getElementsByClassName("progress-bar")[0].style = "width: 75%";
-        document.getElementsByClassName("progress-bar")[1].style = "width: 100%";
-        document.getElementsByClassName("progress-bar")[2].style = "width: 50%";
-    }
+    // skills bars
+    document.querySelectorAll(".progress-bar")[0].style = projectObject.firstBar[currentImgIndex];
+    document.querySelectorAll(".progress-bar")[1].style = projectObject.secondBar[currentImgIndex];
+    document.querySelectorAll(".progress-bar")[2].style = projectObject.thirdBar[currentImgIndex];
 }
+
