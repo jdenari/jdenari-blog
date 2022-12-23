2f413a809eac09126792c7330a68667495c5a7a3
João Vitor Denari dos Santos <98748740+jdenari@users.noreply.github.com>
2022-12-23 11:51:44 -0300

    new-pojects-added

--- js/javascript.js
+++ js/javascript.js
@@ -1,31 +1,35 @@
 // all variables object
 projectObject = {
     
-    name: ['Denari Blog', 'Metaseed', 'Counter Strike GO Website'],
+    name: ['Denari Blog', 'Metaseed Landing Page', 'Building Project Portfolio', 'RouteHelper', 'Metaseed'],
     
-    firstDescription: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+    firstDescription: [
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+        'The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software.',
+        'Richard McClintock, a Latin scholar from Hampden-Sydney College, is credited with discovering the source behind the ubiquitous filler text.',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+        'Richard McClintock, a Latin scholar from Hampden-Sydney College, is credited with discovering the source behind the ubiquitous filler text.',
+    ],
+
+    secondDescription: [
+        'The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software. Today it\'s seen all around the web; on templates, websites, and stock designs.',
+        'In particular, the garbled words of lorem ipsum bear an unmistakable resemblance to sections',
+        'The placeholder text, beginning with the line “Lorem ipsum dolor sit amet, consectetur adipiscing elit”, looks like Latin because in its youth, centuries ago, it was Latin.',
+        'In particular, the garbled words of lorem ipsum bear an unmistakable resemblance to sections',
+        'The placeholder text, beginning with the line “Lorem ipsum dolor sit amet, consectetur adipiscing elit”, looks like Latin because in its youth, centuries ago, it was Latin.',
+    ],
+
+    firstIcon: ['HTML', 'HTML', 'HTML', 'VUEJS', 'HTML'],
+
+    secondIcon: ['BOOTSTRAP', 'CSS', 'CSS', 'NODEJS', 'HTML'],
+
+    thirdIcon: ['JAVASCRIPT', 'JAVASCRIPT', 'JAVASCRIPT', 'MONGODB', 'HTML'],
+
+    firstBar: ['width: 50%', 'width: 50%', 'width: 75%', 'width: 75%', 'width: 50%'],
     
-    'The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software.',
-    
-    'Richard McClintock, a Latin scholar from Hampden-Sydney College, is credited with discovering the source behind the ubiquitous filler text.'],
+    secondBar: ['width: 75%', 'width: 75%', 'width: 100%', 'width: 75%', 'width: 50%'],
 
-    secondDescription: ['The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software. Today it\'s seen all around the web; on templates, websites, and stock designs.',
-    
-    'In particular, the garbled words of lorem ipsum bear an unmistakable resemblance to sections',
-    
-    'The placeholder text, beginning with the line “Lorem ipsum dolor sit amet, consectetur adipiscing elit”, looks like Latin because in its youth, centuries ago, it was Latin.'],
-
-    firstIcon: ['HTML', 'HTML', 'HTML'],
-
-    secondIcon: ['BOOTSTRAP', 'CSS', 'CSS'],
-
-    thirdIcon: ['JAVASCRIPT', 'JAVASCRIPT', 'JAVASCRIPT'],
-
-    firstBar: ["width: 50%", "width: 50%", "width: 75%"],
-    
-    secondBar: ['width: 75%', 'width: 75%', 'width: 100%'],
-
-    thirdBar: ['width: 25%', 'width: 100%', 'width: 50%'],
+    thirdBar: ['width: 25%', 'width: 100%', 'width: 50%', 'width: 75%', 'width: 50%'],
 }
 
 // index to guide the changes
@@ -36,11 +40,11 @@ const addIndex = () => currentImgIndex++;
 const subIndex = () => currentImgIndex--;
 
 function checkIndex(){
-    if (currentImgIndex === 3){
+    if (currentImgIndex === 5){
         currentImgIndex=0;
     } 
     if (currentImgIndex === -1){
-        currentImgIndex=2;
+        currentImgIndex=4;
     }
 }
 
@@ -48,6 +52,8 @@ function checkIndex(){
 const changeIndexto0 = () => currentImgIndex=0;
 const changeIndexto1 = () => currentImgIndex=1;
 const changeIndexto2 = () => currentImgIndex=2;
+const changeIndexto3 = () => currentImgIndex=3;
+const changeIndexto4 = () => currentImgIndex=3;
 
 // next or prev project function
 function changeProject(){
